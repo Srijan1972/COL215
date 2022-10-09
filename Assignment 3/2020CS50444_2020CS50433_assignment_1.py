95ed5f0b86f6810ae14beb16722c609283f77249
Srijan1972 <srijang@live.com>
2022-10-10 00:33:02 +0530

    Report 2.2

--- Assignment 3/2020CS50444_2020CS50433_assignment_1.py
+++ Assignment 3/2020CS50444_2020CS50433_assignment_1.py
@@ -1,15 +1,24 @@
 def is_legal_region(kmap_function, term):
     """
-    determines whether the specified region is LEGAL for the K-map function
-    Arguments:
+    Determines whether the specified region is LEGAL for the K-map function
+
+    Parameters:
     kmap_function: n * m list containing the kmap function
-    for 2-input kmap this will 2*2
-    3-input kmap this will 2*4
-    4-input kmap this will 4*4
-    term: a list of size k, where k is the number of inputs in function (2,3 or 4)
-    (term[i] = 0 or 1 or None, corresponding to the i-th variable)
-    Return:
-    three-tuple: (top-left coordinate, bottom right coordinate, boolean value)
-    each coordinate is represented as a 2-tuple
+        2-input kmap -> 2*2
+        3-input kmap -> 2*4
+        4-input kmap -> 4*4
+    term: a list of size k, where k is the number of inputs in function
+        term[i] = 0 | 1 | None
+
+    Returns:
+    Three-tuple: (top-left coordinate, bottom right coordinate, legality)
     """
+    num = len(term)
+    n = len(kmap_function)
+    m = len(kmap_function[0])
+    assert(2**num==n*m),"Kmap & term size do not match"
+    legal = True
+    tl = None
+    br = None
+    return (tl,br,legal)
     # your code here
