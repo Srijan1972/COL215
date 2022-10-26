646c52b00a1c3c911b31d1d3f42cfd8a0d0c92be
Srijan1972 <srijang@live.com>
2022-10-26 23:48:32 +0530

    Convert Terms to Int

--- Assignment 4/2020CS50444_2020CS50433_assignment_2.py
+++ Assignment 4/2020CS50444_2020CS50433_assignment_2.py
@@ -1,3 +1,4 @@
+from sqlalchemy import true
 from K_map_gui_tk import *
 
 def comb_function_expansion(func_TRUE, func_DC):
@@ -21,28 +22,30 @@ def comb_function_expansion(func_TRUE, func_DC):
     true_bin = []
     dc_bin = []
     for term in func_TRUE:
-        bin = []
+        bin = 0
         for var in vars:
             i = term.find(var)
             if i<len(term)-1 and term[i+1]=="'":
-                bin.append(0)
+                bin = bin << 1
             else:
-                bin.append(1)
+                bin = (bin << 1) + 1
         true_bin.append(bin)
     for term in func_DC:
-        bin = []
+        bin = 0
         for var in vars:
             i = term.find(var)
             if i<len(term)-1 and term[i+1]=="'":
-                bin.append(0)
+                bin = bin << 1 
             else:
-                bin.append(1)
+                bin = (bin << 1) + 1
         dc_bin.append(bin)
+    print(true_bin)
+    print(dc_bin)
     return ans
     
 
 func_TRUE = ["a'b'c'd'e'", "a'b'cd'e", "a'b'cde'", "a'bc'd'e'", "a'bc'd'e", "a'bc'de", "a'bc'de'", "ab'c'd'e'", "ab'cd'e'"]
 func_DC = ["abc'd'e'", "abc'd'e", "abc'de", "abc'de'"]
-# func_TRUE = ["a'bc'd'", "abc'd'", "a'b'c'd", "a'bc'd", "a'b'cd"]
-# func_DC = ["abc'd"]
+func_TRUE = ["a'bc'd'", "abc'd'", "a'b'c'd", "a'bc'd", "a'b'cd"]
+func_DC = ["abc'd"]
 print(comb_function_expansion(func_TRUE,func_DC))
