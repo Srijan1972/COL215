a5d79b05681d0610c3856279701679fb1727dac1
NischayDiwan <nischaydiwan1026@gmail.com>
2022-10-27 20:11:00 +0530

    updates

--- Assignment 4/2020CS50444_2020CS50433_assignment_2.py
+++ Assignment 4/2020CS50444_2020CS50433_assignment_2.py
@@ -1,5 +1,45 @@
-from sqlalchemy import true
 from K_map_gui_tk import *
+
+getbinary = lambda x, n: format(x, 'b').zfill(n)
+
+def xorcheck(x,y):
+    p = 0
+    for m in x:
+        p = p | m
+    q = 0
+    for n in y:
+        q = q | n
+    r = p ^ q
+    return r & (r-1) == 0
+
+def get_term(term_list,vars):
+    numvars = len(vars)
+    term_str = ""
+    term_list.sort()
+    x = term_list[0] ^ term_list[-1]
+    y = getbinary(x,numvars)
+    z = getbinary(term_list[0],numvars)
+    for i in range(numvars):
+        if(y[i] == "0"):
+            if(z[i] == "1"):
+                term_str = term_str + vars[i]
+            else:
+                term_str = term_str + vars[i] + "\'"
+    return term_str
+
+def expandterm(term,true_bin,dc_bin,vars):
+    print("current term expansion :",get_term(term,vars))
+    x = term
+    res = x
+    for y in true_bin:
+        if(x != y and xorcheck(x,y)):
+            print("next legal terms :",get_term(y,vars))
+            print("yems",x,y)
+            res = x + y
+        elif(x != y):
+            print("nos",x,y)
+    print("expanded term :",get_term(res,vars))
+    return res
 
 def comb_function_expansion(func_TRUE, func_DC):
     """
@@ -29,7 +69,7 @@ def comb_function_expansion(func_TRUE, func_DC):
                 bin = bin << 1
             else:
                 bin = (bin << 1) + 1
-        true_bin.append(bin)
+        true_bin.append([bin])
     for term in func_DC:
         bin = 0
         for var in vars:
@@ -38,14 +78,24 @@ def comb_function_expansion(func_TRUE, func_DC):
                 bin = bin << 1 
             else:
                 bin = (bin << 1) + 1
-        dc_bin.append(bin)
+        dc_bin.append([bin])
+    print(func_TRUE)
     print(true_bin)
+    print(func_DC)
     print(dc_bin)
+    for x in true_bin:
+        ans.append(get_term(expandterm(x,true_bin,dc_bin,vars),vars))
     return ans
     
 
-func_TRUE = ["a'b'c'd'e'", "a'b'cd'e", "a'b'cde'", "a'bc'd'e'", "a'bc'd'e", "a'bc'de", "a'bc'de'", "ab'c'd'e'", "ab'cd'e'"]
-func_DC = ["abc'd'e'", "abc'd'e", "abc'de", "abc'de'"]
+# func_TRUE = ["a'b'c'd'e'", "a'b'cd'e", "a'b'cde'", "a'bc'd'e'", "a'bc'd'e", "a'bc'de", "a'bc'de'", "ab'c'd'e'", "ab'cd'e'"]
+# func_DC = ["abc'd'e'", "abc'd'e", "abc'de", "abc'de'"]
 func_TRUE = ["a'bc'd'", "abc'd'", "a'b'c'd", "a'bc'd", "a'b'cd"]
 func_DC = ["abc'd"]
 print(comb_function_expansion(func_TRUE,func_DC))
+print("expected output:",["bc'","bc'","a'c'd","bc'","a'b'd"])
+# L = [0,8]
+# L2 = [4,12]
+# print(get_term(L,["a","b","c","d"]))
+# print(get_term(L2,["a","b","c","d"]))
+# print(xorcheck(L,L2))
